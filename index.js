d0e8fdfef2f1b949b952bc011dac6ee6ff4d6bdf
Kyle <kyleliang586@gmail.com>
2022-10-11 11:00:35 -0700

    commit tree 5592ced5aa48f7960f1ea9a70a4f0e2c3cacb6cc

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph) {
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat(Bob) {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(Milo, Otis) {
+    return cats.pop(["Milo", "Otis"])
+}
+
+function destructivelyRemoveFirstCat(Otis, Garfield) {
+    return cats.shift(["Otis", "Garfield"])
+}
+function appendCat(Broom){
+    return appendCat = [...cats, "Broom"]
+}
+function prependCat(Arnold){
+    return prependCat = ["Arnold", ...cats]
+}
+function removeLastCat() {
+    return removeLastCat= [...cats.slice(0, -1)]
+}
+function removeFirstCat() {
+    return removeFirstCat = [...cats.slice(1)]
+}
